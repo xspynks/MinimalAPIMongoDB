16e145fdc16189ba3f84f13484c07008cfd9859f
Rafael de Paula <xspynks@gmail.com>
2022-11-25 01:47:02 -0300

    feat: create database settings

---
+++ Data/DeveloperDatabaseSettings.cs
@@ -0,0 +1,8 @@
+namespace MinimalAPIMongoDB.Data;
+
+public class DeveloperDatabaseSettings
+{
+    public string ConnectionString { get; set; }
+    public string DatabaseName { get; set; }
+    public string CollectionName { get; set; }
+}
